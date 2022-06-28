1fbf4881e694e94614aef5d726cc5ba87283a6eb
Supakit <supakit.pnm@gmail.com>
2022-06-28 15:38:24 +0700

    Starting Project

---
+++ basic-testing-frontend/src/math.test.js
@@ -0,0 +1,56 @@
+import { it, expect } from "vitest";
+import { add } from "./math";
+
+it("should summarize all number values in array", () => {
+  //Arrange
+  const numbers = [1, 2, 3];
+
+  //Act
+  const result = add(numbers);
+
+  //Assert
+  const expectedResult = numbers.reduce(
+    (preValue, curValue) => preValue + curValue,
+    0
+  );
+  expect(result).toBe(expectedResult);
+});
+
+it("should yield NaN if a least one invalid number is provide", () => {
+  //Arrange
+  const numbers = ["invalid", 1, 2];
+
+  //Act
+  const result = add(numbers);
+
+  //Assert
+  expect(result).toBeNaN();
+});
+
+it("should yield a correct sum if an array of numeric string value is provied", () => {
+  const numbers = ["1", "2"];
+
+  const result = add(numbers);
+
+  const expectedResult = numbers.reduce(
+    (preValue, curValue) => preValue + +curValue,
+    0
+  );
+  expect(result).toBe(expectedResult);
+});
+
+it("should yield 0 if an empty array is provided", () => {
+  const numbers = [];
+
+  const result = add(numbers);
+
+  expect(result).toBe(0);
+});
+
+it("should throw an error if no value is passed into function", () => {
+  const resultFM = () => {
+    add();
+  };
+
+  expect(resultFM).toThrow();
+});
