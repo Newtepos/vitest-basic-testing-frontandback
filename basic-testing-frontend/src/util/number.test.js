ea86189b28974067967a5b3a128a02204c37a77c
Supakit <supakit.pnm@gmail.com>
2022-06-29 23:17:54 +0700

    Add: number test

--- basic-testing-frontend/src/util/number.test.js
+++ basic-testing-frontend/src/util/number.test.js
@@ -1,21 +1,40 @@
-import { expect, it } from "vitest";
-import { transformToNumber } from "./numbers";
+import { expect, it, describe } from "vitest";
+import { cleanNumbers, transformToNumber } from "./numbers";
 
-it("shoud transfrom a string number to number of type number", () => {
-  const input = "1";
+describe("transformToNumber", () => {
+  it("shoud transfrom a string number to number of type number", () => {
+    const input = "1";
 
-  const result = transformToNumber(input);
+    const result = transformToNumber(input);
 
-  expect(result).toBeTypeOf("number");
+    expect(result).toBeTypeOf("number");
+  });
+
+  it("should yield NaN for non-transformable value", () => {
+    const input = "invalid";
+    const input2 = {};
+
+    const result = transformToNumber(input);
+    const result2 = transformToNumber(input2);
+
+    expect(result).toBeNaN();
+    expect(result2).toBeNaN();
+  });
 });
 
-it("should yield NaN for non-transformable value", () => {
-  const input = "invalid";
-  const input2 = {};
+describe("cleanNumbers", () => {
+  it("should return an array of number values if an array of string number values is provied", () => {
+    const numbers = ["1", "2"];
 
-  const result = transformToNumber(input);
-  const result2 = transformToNumber(input2);
+    const cleanedNumbers = cleanNumbers(numbers);
 
-  expect(result).toBeNaN();
-  expect(result2).toBeNaN();
+    expect(cleanedNumbers[0]).toBeTypeOf("number");
+  });
+
+  it("should throw an error if an array with at least one empty string is provided", () => {
+    const numbers = ["", 1];
+    const cleanFn = () => cleanNumbers(numbers);
+
+    expect(cleanFn).toThrow();
+  });
 });
